6c31193dcab7758f2d1dbfd85ffdaea14de6d32c
LiuvaSalas <liuvasalas01@gmail.com>
2024-09-09 22:07:02 -0300

    generamos los inputs para que el usuario pueda ingresar el nuevo nombre de la campaña y el nuevo tipo de anuncio dentreo de los disponibles.

--- demo.py
+++ demo.py
@@ -6,3 +6,7 @@ campanha = Campana("Campaña1", listado_anuncios, "x", "y")
 
 #probamos que se crea la campaña
 print(campanha)
+
+#solicitamos el nombre nuevo que el usuario desea asignar a la campaña anteriormente impresa y modificamos el subtipo
+nuevo_nombre = input("Ingrese el nuevo nombre de la campaña: ")
+nuevo_sub_tipo = input("Ingrese el nuevo subtipo para el anuncio de video: ")
